e6478d2ac91e424c7639c6fe5044f0f14ae32011
matry18 <matry18@student.sdu.dk>
2021-10-16 14:14:21 +0200

    TestFetch

--- src/test/java/com/example/AdifyServiceTest.java
+++ src/test/java/com/example/AdifyServiceTest.java
@@ -31,10 +31,20 @@ class AdifyServiceTest {
   @Tag("slow")
   void fetchProductPageEvent_sendsDisplayEvent() {
     SenderSpy spy = new SenderSpy();
-    //spy.send("fetch-product-page","SESSION_ID,advert,PRODUCT_ID,PRODUCT_NAME");
     AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), "SESSION_ID,USER_ID,PRODUCT_ID", spy);
     a.execute();
     assertEquals("SESSION_ID,advert,PRODUCT_ID,PRODUCT_NAME", spy.body);
+  }
+
+  @Test
+  @Tag("fast")
+  void adifyServiceExecute_gatefetchNotNull() {
+    SenderSpy spy = new SenderSpy();
+    AdifyService a = new AdifyService(new Adify(new HerokuGetRequest("adify")), "SESSION_ID,USER_ID,PRODUCT_ID", spy);
+    a.execute();
+    assertEquals("SESSION_ID", spy.body.split(",")[0]);
+    assertEquals("USER_ID", spy.body.split(",")[1]);
+    assertEquals("PRODUCT_ID", spy.body.split(",")[2]);
   }
 
 }
